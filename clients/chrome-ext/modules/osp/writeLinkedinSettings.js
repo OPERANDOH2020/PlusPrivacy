39575e981c7563167565f9befbb7a009fa4da90c
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2017-07-04 18:09:04 +0300

    speed-ed up the process of applying settings

--- clients/chrome-ext/modules/osp/writeLinkedinSettings.js
+++ clients/chrome-ext/modules/osp/writeLinkedinSettings.js
@@ -13,6 +13,7 @@
 
 var privacySettings = [];
 var port = chrome.runtime.connect({name: "applyLinkedinSettings"});
+var extractedData = {};
 port.postMessage({action: "waitingLinkedinCommand", data: {status:"waitingLinkedinCommand"}});
 
 port.onMessage.addListener(function (msg) {
@@ -33,124 +34,133 @@ function postToLinkedIn(settings, item, total) {
 
         if (settings.page) {
             FeedbackProgress.sendFeedback(settings.name, item, total);
-            doGET(settings.page, function (response) {
 
-                var headers = extractHeaders(response);
-
-                var data = {};
-
-                chrome.runtime.sendMessage({
-                    message: "getCookies",
-                    url: settings.page
-                }, function (response) {
-                    var cookies = "";
-                    for (var i = 0; i < response.length; i++) {
-                        cookies += response[i].name + "=" + response[i].value + "; ";
-                    }
-
-
-                    for (var prop in settings.data) {
-                        data[prop] = settings.data[prop];
-                    }
-
-
-                    for (var param in settings.params) {
-                        if (settings.params[param].type && settings.params[param].type === "dynamic") {
-                            if (headers[param]) {
-                                settings.url = settings.url.replace("{" + settings.params[param].placeholder + "}", headers[param]);
-                            }
-                        }
-                    }
-                    console.log(data);
-                    if (settings.type == "application/json") {
-                        $.ajax({
-                            type: "POST",
-                            url: settings.url,
-                            data: JSON.stringify(data),
-                            contentType: 'application/json; charset=utf-8',
-                            dataType: "json",
-                            beforeSend: function (request) {
-                                if (settings.headers) {
-                                    for (var i = 0; i < settings.headers.length; i++) {
-                                        var header = settings.headers[i];
-                                        request.setRequestHeader(header.name, header.value);
-                                    }
-                                }
-
-                                request.setRequestHeader("accept", "application/json, text/javascript, */*; q=0.01");
-                                request.setRequestHeader("accept-language", "en-US,en;q=0.8");
-                                request.setRequestHeader("X-Alt-Referer", settings.page);
-
-                            },
-                            success: function (result) {
-                                resolve(result);
-                            },
-                            error: function (a, b, c) {
-                                console.log(a, b, c);
-                                reject(b);
-                            },
-                            complete: function (request, status) {
-                                console.log("Request completed...");
-                            }
-
-                        });
-                    }
-                    else {
-
-                        var formData = new FormData();
-                        for (var p in headers) {
-                            formData.append(p, headers[p]);
-                        }
-
-                        for (var prop in data) {
-                            formData.append(prop, data[prop]);
-                        }
-                        $.ajax({
-                            type: "POST",
-                            url: settings.url,
-                            data: formData,
-                            contentType: false,
-                            processData: false,
-
-                            beforeSend: function (request) {
-
-                                if (settings.headers) {
-                                    for (var i = 0; i < settings.headers.length; i++) {
-                                        var header = settings.headers[i];
-                                        request.setRequestHeader(header.name, header.value);
-                                    }
-                                }
-                                request.setRequestHeader("accept", "*/*");
-                                request.setRequestHeader("accept-language", "en-US,en;q=0.8");
-                                request.setRequestHeader("X-Alt-Referer", settings.page);
-                            },
-                            success: function (result) {
-                                setTimeout(function(){
-                                    resolve(result);
-                                },0);
-                            },
-                            statusCode:{
-                              500: function(){
-                                  console.log("Sunt in 500");
-                              }
-                            },
-                            error: function (a, b, c) {
-                                console.log(a, b, c);
-                                reject(b);
-                            },
-                            complete: function (request, status) {
-                                console.log("Request completed...");
-                            }
-
-                        });
-                    }
+            if(Object.keys(extractedData).length === 0){
+                doGET(settings.page, function (response) {
+                    var additionalData = extractHeaders(response);
+                    sendPostRequest(settings,additionalData,resolve,reject);
                 });
+            }
+            else{
+                sendPostRequest(settings,extractedData,resolve,reject);
+            }
 
-            })
         }
 
     });
+}
 
+function sendPostRequest(settings, headers, resolve, reject){
+
+    var data = {};
+    chrome.runtime.sendMessage({
+        message: "getCookies",
+        url: settings.page
+    }, function (response) {
+        var cookies = "";
+        for (var i = 0; i < response.length; i++) {
+            cookies += response[i].name + "=" + response[i].value + "; ";
+        }
+
+
+        for (var prop in settings.data) {
+            data[prop] = settings.data[prop];
+        }
+
+
+        for (var param in settings.params) {
+            if (settings.params[param].type && settings.params[param].type === "dynamic") {
+                if (headers[param]) {
+                    settings.url = settings.url.replace("{" + settings.params[param].placeholder + "}", headers[param]);
+                }
+            }
+        }
+        if (settings.type == "application/json") {
+            $.ajax({
+                type: "POST",
+                url: settings.url,
+                data: JSON.stringify(data),
+                contentType: 'application/json; charset=utf-8',
+                dataType: "json",
+                beforeSend: function (request) {
+                    if (settings.headers) {
+                        for (var i = 0; i < settings.headers.length; i++) {
+                            var header = settings.headers[i];
+                            request.setRequestHeader(header.name, header.value);
+                        }
+                    }
+
+                    request.setRequestHeader("accept", "application/json, text/javascript, */*; q=0.01");
+                    request.setRequestHeader("accept-language", "en-US,en;q=0.8");
+                    request.setRequestHeader("X-Alt-Referer", settings.page);
+
+                },
+                success: function (result) {
+                    resolve(result);
+                },
+                error: function (a, b, c) {
+                    console.log(a, b, c);
+                    reject(b);
+                },
+                complete: function (request, status) {
+                    console.log("Request completed...");
+                },
+                timeout:3000
+
+            });
+        }
+        else {
+
+            var formData = new FormData();
+            for (var p in headers) {
+                formData.append(p, headers[p]);
+            }
+
+            for (var prop in data) {
+                formData.append(prop, data[prop]);
+            }
+            $.ajax({
+                type: "POST",
+                url: settings.url,
+                data: formData,
+                contentType: false,
+                processData: false,
+
+                beforeSend: function (request) {
+
+                    if (settings.headers) {
+                        for (var i = 0; i < settings.headers.length; i++) {
+                            var header = settings.headers[i];
+                            request.setRequestHeader(header.name, header.value);
+                        }
+                    }
+                    request.setRequestHeader("accept", "*/*");
+                    request.setRequestHeader("accept-language", "en-US,en;q=0.8");
+                    request.setRequestHeader("X-Alt-Referer", settings.page);
+                },
+                success: function (result) {
+                    setTimeout(function(){
+                        resolve(result);
+                    },0);
+                },
+                statusCode:{
+                    500: function(){
+                        console.log("500 error");
+                        reject();
+                    }
+                },
+                error: function (a, b, c) {
+                    console.log(a, b, c);
+                    reject(b);
+                },
+                complete: function (request, status) {
+                    console.log("Request completed...");
+                },
+                timeout:3000
+
+            });
+        }
+    });
 }
 
 function secureAccount(callback) {
@@ -177,34 +187,12 @@ function secureAccount(callback) {
 
 }
 
-
-/*function doGET(page, callback) {
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", page, true);
-    try{
-        xmlHttp.send(null);
-    }
-    catch (e){
-        console.log(e);
-
-        setTimeout(function(){
-            doGET(page, callback);
-        },1000);
-    }
-
-}
-*/
 function doGET(page, callback){
-    $.ajax({
-        url: page,
-        success: callback,
-        dataType: 'html'
-    });
+        $.ajax({
+            url: page,
+            success: callback,
+            dataType: 'html'
+        });
 }
 
 
@@ -219,6 +207,6 @@ function extractHeaders(content) {
         }
     }
     data['csrfToken'] = decodeURIComponent(match[1]);
+    extractedData = data;
     return data;
-
 }
