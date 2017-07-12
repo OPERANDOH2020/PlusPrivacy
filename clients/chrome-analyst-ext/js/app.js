08619462368dd695927f3fea731ba4e1e9ce4b6b
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2017-07-12 16:38:23 +0300

    added login screen

--- clients/chrome-analyst-ext/js/app.js
+++ clients/chrome-analyst-ext/js/app.js
@@ -1,3 +1,46 @@
-/**
- * Created by Rafa on 7/12/2017.
- */
+var app = angular.module("app", ['angularModalService','ui.router','oc.lazyLoad']);
+
+
+app.filter('isEmpty', [function () {
+    return function (object) {
+        return angular.equals({}, object);
+    }
+}]);
+
+app.
+config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
+
+
+app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+
+    $ocLazyLoadProvider.config({
+        debug: false,
+        serie: true,
+        cache: false
+    });
+
+    $stateProvider
+        .state("login", {
+            url: "/",
+            templateUrl: "../templates/login.html",
+            resolve:{
+                loadController: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load('/js/controllers/loginController.js');
+                }]
+            }
+        })
+        .state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "../assets/templates/register_osp.html",
+            resolve:{
+                loadController: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load('/app/controllers/ospSignupController.js');
+                }]
+            }
+        });
+
+    $urlRouterProvider.otherwise("/");
+
+});
