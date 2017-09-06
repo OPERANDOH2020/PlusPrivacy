8d07eae80d5e73bfef3374ad5879fdb38d667934
mastaleru <mastaleru.rafael@gmail.com>
2017-09-07 00:02:55 +0300

    added feedback swarm

---
+++ servers/coreServer/operando/swarms/feedback.js
@@ -0,0 +1,46 @@
+var feedbackSwarming = {
+
+    getFeedbackQuestions: function () {
+        this.swarm("getFeedbackFormQuestions")
+    },
+    submitFeedback: function (feedback) {
+        this.feedback = feedback;
+        this.swarm("submitFeedbackValues")
+    },
+
+    getFeedbackFormQuestions: {
+        node: "FeedbackAdapter",
+        code: function () {
+            var self = this;
+            getFeedbackQuestions(S(function (err, feedbackQuestions) {
+                if (err) {
+                    console.error(err);
+                    self.home("error");
+                }
+                else {
+                    self.feedbackQuestions = feedbackQuestions;
+                    self.home("success");
+                }
+            }));
+        }
+    },
+
+    submitFeedbackValues: {
+        node: "FeedbackAdapter",
+        code: function () {
+            var self = this;
+            submitFeedbackAnswer(this.feedback,S(function(err, feedback){
+                if (err) {
+                    console.error(err);
+                    self.home("error");
+                }
+                else {
+                    self.home("success");
+                }
+            }));
+        }
+    }
+
+
+};
+feedbackSwarming;
