c6afb45dcc6b51df58d9a59bde25f0197d680bac
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2017-09-08 18:30:08 +0300

    feedback form work

--- clients/chrome-ext/controllers/feedbackController.js
+++ clients/chrome-ext/controllers/feedbackController.js
@@ -1,8 +1,43 @@
 angular.module("operando").
 controller("FeedbackController", ["$scope","$sce", "messengerService", function ($scope, $sce, messengerService) {
 
+    $scope.answers = {};
+
     messengerService.send("provideFeedbackQuestions", function(response){
         $scope.feedbackQuestions = response.data;
-        console.log($scope.feedbackQuestions);
+        prepareResponses($scope.feedbackQuestions);
+        $scope.$apply();
     });
+
+
+    function prepareResponses(questions){
+
+        questions.forEach(function(question){
+            switch (question.type){
+                case "multipleRating" :
+                    question.items.forEach(function(item){
+                        $scope.answers[question.title+"["+item+"]"] = "";
+                    });
+                    break;
+                case "multipleSelection" :
+                    question.items.forEach(function(item){
+                        $scope.answers[question.title+"["+item+"]"] = "";
+                    });
+                    break;
+                case "textInput" :
+                    $scope.answers[question.title] = "";
+                    break;
+                case "radio" :
+                    $scope.answers[question.title] = "";
+                    break;
+
+            }
+        });
+
+    }
+
+    $scope.submitFeedback = function(){
+        console.log($scope.answers);
+    }
+
 }]);
