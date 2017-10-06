7c108ff424dd91fbd231d82cf6afb4a35225230e
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2017-10-06 17:22:48 +0300

    added notifications services

--- clients/chrome-analyst-ext/js/config.js
+++ clients/chrome-analyst-ext/js/config.js
@@ -9,6 +9,8 @@ function Config(){
             email:"guest@operando.eu",
             password:"guest",
             tenant:"GUEST"
-        }
+        },
+        SERVER_HOST_PROTOCOL:"http",
+        WEBSITE_HOST:"localhost"
     };
 };
