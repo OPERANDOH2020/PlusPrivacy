5205ae367f5dddeec76cfad6832a7556ee17d88f
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2017-10-18 12:14:51 +0300

    config

--- servers/coreServer/admin/administration-console/config_production.js
+++ servers/coreServer/admin/administration-console/config_production.js
@@ -4,7 +4,7 @@ function Config(){
         brand: "{+P} Admin Console",
         mobileDebug: true,
         swarmClient: {
-            host: "localhost",
+            host: "plusprivacy.com",
             port: 8080,
             tenant: "admin"
         },
