975b9672e1df79d21a1f042e6e2fb4ce4bdbbb96
CristiSava <crs@rms.ro>
2017-11-28 13:45:17 +0200

    -updated dashboard icons

--- clients/ios/Operando/Operando/UserInterface/UIDashboardButton/UIDashboardButton.swift
+++ clients/ios/Operando/Operando/UserInterface/UIDashboardButton/UIDashboardButton.swift
@@ -13,13 +13,13 @@ struct UIDashboardButtonStyle{
     let title: String?
     let image: UIImage?
     
-    static let identityManagementStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: .operandoDarkGreen, title: Bundle.localizedStringFor(key: kIdentitiesManagementLocalizableKey), image: UIImage(named: "identitiesIcon"))
+    static let identityManagementStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: .operandoDarkGreen, title: Bundle.localizedStringFor(key: kIdentitiesManagementLocalizableKey), image: UIImage(named: "ic_group_white"))
     
-    static let privacyForBenefitsStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: UIColor.operandoRed, title: Bundle.localizedStringFor(key: kPrivacyForBenefitsLocalizableKey), image: UIImage(named: "dealsIcon"))
+    static let privacyForBenefitsStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: UIColor.operandoRed, title: Bundle.localizedStringFor(key: kPrivacyForBenefitsLocalizableKey), image: UIImage(named: "ic_fingerprint_white"))
     
-    static let privateBrowsingStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: UIColor.operandoOrange, title: Bundle.localizedStringFor(key: kPrivateBrowsingLocalizableKey), image: UIImage(named: "browsingIcon"))
+    static let privateBrowsingStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: UIColor.operandoOrange, title: Bundle.localizedStringFor(key: kPrivateBrowsingLocalizableKey), image: UIImage(named: "ic_open_in_browser_white"))
     
-    static let notificationsStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: UIColor.operandoLightGreen, title: Bundle.localizedStringFor(key: kNotificationsLocalizableKey), image: UIImage(named: "notificationsIcon"))
+    static let notificationsStyle: UIDashboardButtonStyle = UIDashboardButtonStyle(backgroundColor: UIColor.operandoLightGreen, title: Bundle.localizedStringFor(key: kNotificationsLocalizableKey), image: UIImage(named: "ic_notifications_white"))
     
 }
 
