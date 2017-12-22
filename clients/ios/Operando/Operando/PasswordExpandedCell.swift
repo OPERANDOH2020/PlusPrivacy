a1ce8a4a53d833703ebf794ab62a1950454f51fa
CristiSava <crs@rms.ro>
2017-12-22 15:00:03 +0200

    - Working on change password logic

--- clients/ios/Operando/Operando/PasswordExpandedCell.swift
+++ clients/ios/Operando/Operando/PasswordExpandedCell.swift
@@ -8,19 +8,35 @@
 
 import UIKit
 
-class PasswordExpandedCell: UITableViewCell {
+class PasswordExpandedCell: UITableViewCell, UITextFieldDelegate {
     
     static let identifierNibName = "PasswordExpandedCell"
 
+    @IBOutlet weak var confirmPassTF: UITextField!
+    @IBOutlet weak var newPassTF: UITextField!
+    @IBOutlet weak var currentPassTF: UITextField!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        confirmPassTF.delegate = self
+        newPassTF.delegate = self
+        currentPassTF.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+//        textField.text stringByReplacingCharactersInRange:range withString:string
+        
+        let string = NSString(string: textField.text!).replacingCharacters(in: range, with: string)
+        
+        
+        return true
     }
     
 }
