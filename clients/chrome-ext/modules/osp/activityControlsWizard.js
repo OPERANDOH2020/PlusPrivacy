563b4cf8250f576edc8848b3219d2ab4b85493ee
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-01-19 17:16:49 +0200

    wip

--- clients/chrome-ext/modules/osp/activityControlsWizard.js
+++ clients/chrome-ext/modules/osp/activityControlsWizard.js
@@ -5,7 +5,7 @@
         var itemHeight = $(this).height();
         $('html, body').animate({
             scrollTop: $(this).offset().top-(windowHeight-itemHeight)/2 + 'px'
-        }, 400);
+        }, 300);
         return this;
     }
 })(jQuery);
@@ -21,7 +21,8 @@
 
 var containerClassName = ".HICA4c";
 var wizardContainer = jQuery("<div class='pp_wizard_container'><div class='pp_logo'></div></div>");
-var stepsContainer = jQuery("<div class='activity_controls_steps'></div>");
+var stepsContainer = jQuery("<div class='activity_controls_steps'><div class='progress_bar'></div></div>");
+var stepsItems = jQuery("<div class='pp_wizard_items'></div>");
 var steps = [];
 var currentIndex = 0;
 var port = chrome.runtime.connect({name: "googleActivityControls"});
@@ -47,23 +48,30 @@ function renderWizard(){
 
 function changeIndex(index){
     jQuery(".pp_wizard_item.active").removeClass("active");
+    jQuery(".pp_wizard_item_"+index).addClass("active");
     jQuery(containerClassName).addClass("pp_item_overlay");
+
+    jQuery(".item_checked").removeClass("item_checked");
+    for(var i = 0; i < index; i++){
+        jQuery(".pp_wizard_item_"+i).addClass("item_checked");
+    }
+
+    jQuery(".progress_bar").height(((index/steps.length)*100).toString()+"%");
     jQuery(jQuery(containerClassName)[index]).removeClass("pp_item_overlay");
     jQuery(jQuery(containerClassName)[index]).goTo();
 }
 
 function prepareWizard(){
     steps.forEach(function(step){
-        var stepContainer = jQuery("<div class='pp_wizard_item'><div class='step_index'>"+step.index+"</div><div class='step_name'>"+step.name+"</div></div>");
+        var stepContainer = jQuery("<div class='pp_wizard_item pp_wizard_item_"+step.index+"'><div class='step_index'>"+step.index+"</div><div class='step_name'>"+step.name+"</div></div>");
         stepContainer.click(function(){
             changeIndex(step.index);
-            stepContainer.addClass("active");
         });
-        stepsContainer.append(stepContainer);
+        stepsItems.append(stepContainer);
     });
 
     var nextBtn = jQuery("<button>Next</button>");
-    var prevBtn = jQuery("<button>Previous</button>");
+    var prevBtn = jQuery("<button disabled='disabled'>Previous</button>");
     var finishBtn = jQuery("<button>Finish</button>");
     var controllers = jQuery("<div class='pp_controller'></div>");
 
@@ -83,11 +91,9 @@ function prepareWizard(){
     nextBtn.click(function(){
         currentIndex ++;
         changeIndex(currentIndex);
-
         if(currentIndex === steps.length-1){
             $(this).attr("disabled","disabled");
         }
-
         if($(prevBtn).hasAttr("disabled")){
             $(prevBtn).removeAttr("disabled");
         }
@@ -96,9 +102,9 @@ function prepareWizard(){
     controllers.append(prevBtn);
     controllers.append(nextBtn);
     controllers.append(finishBtn);
-    stepsContainer.append(controllers);
+    wizardContainer.append(controllers);
 }
-
+stepsContainer.append(stepsItems);
 wizardContainer.append(stepsContainer);
 $("body").append(wizardContainer);
 jQuery(".FaV4Jb").addClass("pp_global_overlay");
