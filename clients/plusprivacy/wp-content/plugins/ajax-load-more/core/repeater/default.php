25304c3efe576f3086e4e13b9f4cd6fbd4decd35
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-02-22 10:02:29 +0200

    added news template

--- clients/plusprivacy/wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ clients/plusprivacy/wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,3 +1,13 @@
+<?php if(has_category('news')==true) :?>
+<li class="news_item">
+	<div class="news_date">
+    	<span class="date-day-month"><?php the_time("F d");?></span>
+    	<span class="date-year"><?php the_time("Y");?></span>
+	</div>
+	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <?php the_excerpt(); ?>
+</li>
+<?php elseif(has_category('blog')==true):?>
 <li<?php if (! has_post_thumbnail() ) { echo ' class="no-img"'; } ?>>
 	<?php
    	if ( has_post_thumbnail() ) {
@@ -6,7 +16,8 @@
 	?>
 	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 	<p class="entry-meta">
-		<?php the_time("F d, Y"); ?>
+		<?php the_time("F d, Y");?>
 	</p>
 	<?php the_excerpt(); ?>
 </li>
+<?php endif;?>
