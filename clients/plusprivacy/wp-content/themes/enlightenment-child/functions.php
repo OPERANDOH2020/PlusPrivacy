396193f1e3b13065ef2a3c9ce7a0df9aaf783820
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-02-28 11:19:08 +0200

    removed emoji canvas

--- clients/plusprivacy/wp-content/themes/enlightenment-child/functions.php
+++ clients/plusprivacy/wp-content/themes/enlightenment-child/functions.php
@@ -26,5 +26,12 @@ function the_excerpt_more_link( $excerpt ){
 }
 add_filter( 'the_excerpt', 'the_excerpt_more_link', 21 );
 
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 ?>
 
