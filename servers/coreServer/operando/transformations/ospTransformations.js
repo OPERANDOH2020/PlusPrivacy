9cce3c278ce79aa6e162a67e1ea3efb770787c74
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-03-10 12:34:27 +0200

    moved swarmDispatcher in swarmCore

--- servers/coreServer/operando/transformations/ospTransformations.js
+++ servers/coreServer/operando/transformations/ospTransformations.js
@@ -1,24 +1,15 @@
-var util = require("swarmcore");
-var adapterPort = 3000;
-var adapterHost = "localhost";
-var assert = require('double-check').assert;
-
 exports.transformations = {
     getOSPSettings: {
         method: 'get',
         params: [],
         path: '/social-networks/privacy-settings',
         code: function (callback) {
-            var uuid = require('uuid');
-            thisAdapter.myUUID = uuid.v1();
-            //TODO register UIID in dispatcher
-            var swarmDispatcher = getSwarmDispatcher();
 
-            return new Promise(function (resolve, reject) {
-                swarmDispatcher.subscribeToSwarmResult(thisAdapter.myUUID, function (data) {
+            var swarmDispatcher = getSwarmDispatcher();
+            return new Promise(function (resolve) {
+                thisAdapter.myUUID = swarmDispatcher.subscribeToSwarmResult(function (data) {
                     resolve(JSON.stringify(data));
                 });
-
                 startSwarm("PrivacyWizardSwarm.js", 'getOSPSettings');
             }).then(function (data) {
                 callback(data);
