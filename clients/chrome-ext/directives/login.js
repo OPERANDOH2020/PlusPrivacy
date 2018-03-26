404f7079699b04a05b4828df732c2ce14d5265a3
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-03-26 18:12:20 +0300

    added feedback to login/signup buttons while the request is processed.

--- clients/chrome-ext/directives/login.js
+++ clients/chrome-ext/directives/login.js
@@ -12,6 +12,7 @@ angular.module("login",[]).
             $scope.new_user = {};
 
             $scope.login = function(){
+                $scope.submitRequest.button('loading');
                 messengerService.send("authenticateUser", {
                         email: $scope.user.email,
                         password: $scope.user.password,
@@ -21,12 +22,15 @@ angular.module("login",[]).
                         Notification.success({message: 'You have successfully logged in!', positionY: 'bottom', positionX: 'center', delay: 2000});
                     }
                     else if (response.error)
+                        $scope.submitRequest.button('reset');
                         Notification.error({message: i18nService._(response.error), positionY: 'bottom', positionX: 'center', delay: 2000});
                 });
             }
 
             $scope.recover_password = function(){
+                $scope.submitRequest.button('loading');
                 messengerService.send("resetPassword", $scope.user['reset_email'], function (response) {
+                    $scope.submitRequest.button('reset');
                     if(response.status === "success"){
                         Notification.success({message: "Check your email!", positionY: 'bottom', positionX: 'center', delay: 3000});
                         delete $scope.user['reset_email'];
@@ -40,7 +44,9 @@ angular.module("login",[]).
             };
 
             $scope.signup = function(){
+                $scope.submitRequest.button('loading');
                 messengerService.send("registerUser",$scope.new_user, function(response){
+                    $scope.submitRequest.button('reset');
                     if(response.status === "success"){
                         Notification.success({message: "Account successfully created! Check your email to confirm registration!", positionY: 'bottom', positionX: 'center', delay: 3000});
                          $scope.user.email = $scope.new_user.email;
@@ -64,6 +70,10 @@ angular.module("login",[]).
 
             $scope.show_forgot_password = function(){
                 $scope.currentView = "forgot_password";
+            }
+
+            $scope.loading = function($event){
+                $scope.submitRequest = $($event.currentTarget);
             }
         }]
 
