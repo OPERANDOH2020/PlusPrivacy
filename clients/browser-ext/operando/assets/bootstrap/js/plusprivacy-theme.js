a2fa94a3378455569c50afce1b92c0acdfb38389
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-05-11 10:55:05 +0300

    small css fixes

--- clients/browser-ext/operando/assets/bootstrap/js/plusprivacy-theme.js
+++ clients/browser-ext/operando/assets/bootstrap/js/plusprivacy-theme.js
@@ -3,49 +3,49 @@ $(document).ready(function () {
      *Custom scrollbar
      */
     (function ($) {
-        $(window).on("load", function () {
-            $("#wrapper").mCustomScrollbar({
-                scrollButtons: {enable: true},
-                theme: "light-thick",
-                scrollbarPosition: "outside",
-                scrollInertia:300,
-                mouseWheelPixels:50,
-                callbacks: {
-                    whileScrolling: function () {
-                        if (this.mcs.top < -100) {
-                            $('#return-to-top').fadeIn();
+        var testChromeBrowser = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
+        if (testChromeBrowser) {
+            $(window).on("load", function () {
+                $("#wrapper").mCustomScrollbar({
+                    scrollButtons: {enable: true},
+                    theme: "light-thick",
+                    scrollbarPosition: "outside",
+                    scrollInertia: 300,
+                    mouseWheelPixels: 50,
+                    callbacks: {
+                        whileScrolling: function () {
+                            if (this.mcs.top < -100) {
+                                $('#return-to-top').fadeIn();
+                            }
+                            else {
+                                $('#return-to-top').fadeOut();
+                            }
+                        },
+                        onOverflowYNone: function () {
+                            $('#return-to-top').hide();
                         }
-                        else {
-                            $('#return-to-top').fadeOut();
-                        }
-                    },
-                    onOverflowYNone: function(){
-                        $('#return-to-top').hide();
                     }
-                }
-            });
-
-            /**
-             *Scroll Up Button
-             */
-            (function ($) {
-
-                window.addEventListener('scrollTop', function (e) {
-                    $("#wrapper").mCustomScrollbar("scrollTo", "top", {
-                        scrollEasing: "easeOut"
-                    });
-                }, false);
-
-                $('#return-to-top').click(function () {
-                    window.dispatchEvent(new Event('scrollTop'));
                 });
 
-            })(jQuery);
+                /**
+                 *Scroll Up Button
+                 */
+                (function ($) {
 
-        });
+                    window.addEventListener('scrollTop', function (e) {
+                        $("#wrapper").mCustomScrollbar("scrollTo", "top", {
+                            scrollEasing: "easeOut"
+                        });
+                    }, false);
 
+                    $('#return-to-top').click(function () {
+                        window.dispatchEvent(new Event('scrollTop'));
+                    });
 
+                })(jQuery);
 
+            });
+        }
     })(jQuery);
 
 });
