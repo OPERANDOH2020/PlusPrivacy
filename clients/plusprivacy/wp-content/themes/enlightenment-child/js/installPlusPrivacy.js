9da006dfcc0ffac9253ffa060f7630a9b83a27a9
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-03-08 16:55:30 +0200

    removed inline install because it is deprecated

--- clients/plusprivacy/wp-content/themes/enlightenment-child/js/installPlusPrivacy.js
+++ clients/plusprivacy/wp-content/themes/enlightenment-child/js/installPlusPrivacy.js
@@ -1,21 +1,8 @@
 jQuery(document).ready(function(){
     jQuery("#download_extension").on("click", function(){
         var self = this;
-        if (document.getElementById('plusprivacy-extension-is-installed')) {
             jQuery(self).attr("href","https://chrome.google.com/webstore/detail/boagbmhcbemflaclmnbeebgbfhbegekc");
             jQuery(self).attr("target","_blank");
             jQuery(self).click();
-        }
-        else{
-            if(window.chrome){
-                chrome.webstore.install("https://chrome.google.com/webstore/detail/boagbmhcbemflaclmnbeebgbfhbegekc", function(){
-                }, function(error){
-                });
-            }
-            else{
-                checkInstallation();
-            }
-
-        }
     });
 });
